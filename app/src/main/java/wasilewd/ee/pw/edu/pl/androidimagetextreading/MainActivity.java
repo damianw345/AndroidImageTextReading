1102662590bb1d67de44b1af0879af368bdbd997
Damian Wasilewski <damian.wasilewski@mobica.com>
2017-12-06 18:16:55 +0100

    import fixes

--- app/src/main/java/wasilewd/ee/pw/edu/pl/androidimagetextreading/MainActivity.java
+++ app/src/main/java/wasilewd/ee/pw/edu/pl/androidimagetextreading/MainActivity.java
@@ -1,21 +1,17 @@
 package wasilewd.ee.pw.edu.pl.androidimagetextreading;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
