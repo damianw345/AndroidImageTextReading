2df4c92a6cbb973939471bd29973195f6d9b9f96
Damian Wasilewski <damian.wasilewski@mobica.com>
2017-12-29 00:04:46 +0100

    Removed black background from image after rotation

--- app/src/main/java/wasilewd/ee/pw/edu/pl/androidimagetextreading/ProcessPhotoActivity.java
+++ app/src/main/java/wasilewd/ee/pw/edu/pl/androidimagetextreading/ProcessPhotoActivity.java
@@ -140,7 +140,10 @@ public class ProcessPhotoActivity extends AbstractTesseractActivity {
     private static Bitmap rotateBitmap(Bitmap source, float angle) {
         Matrix matrix = new Matrix();
         matrix.postRotate(angle);
-        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+
+        Bitmap tmpRotatedBitmap = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+        tmpRotatedBitmap.setHasAlpha(true);
+        return tmpRotatedBitmap;
     }
 }
 
