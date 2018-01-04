ab6f11120f21d7c14286b3b973106d10fe17a596
Damian Wasilewski <damian.wasilewski@mobica.com>
2018-01-04 20:36:50 +0100

    Fixed taking photos on Android version >= 6.0

--- app/src/main/java/wasilewd/ee/pw/edu/pl/androidimagetextreading/TakePhotoActivity.java
+++ app/src/main/java/wasilewd/ee/pw/edu/pl/androidimagetextreading/TakePhotoActivity.java
@@ -1,15 +1,19 @@
 package wasilewd.ee.pw.edu.pl.androidimagetextreading;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,6 +30,9 @@ public class TakePhotoActivity extends AppCompatActivity {
     private String imageFileName;
     private String imageAbsolutePath;
 
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private Intent cameraIntent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,23 +44,68 @@ public class TakePhotoActivity extends AppCompatActivity {
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (cameraIntent.resolveActivity(getPackageManager()) != null) {
-                    // Create the File where the photo should go
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                    } catch (IOException ex) {
-                        System.out.println("IOException");
+
+                    // if Android version >= Android 6.0
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+
+                            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                    Manifest.permission.CAMERA};
+
+                            requestPermissions(permissions, PERMISSION_REQUEST_CODE);
                     }
-                    // Continue only if the File was successfully created
-                    if (photoFile != null) {
-                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-                        startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+
+                    //proceed without asking for permissions
+                    else {
+                        createImageFileAndPassItAsUriToOnActivityResult();
                     }
                 }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the task you need to do.
+                    createImageFileAndPassItAsUriToOnActivityResult();
+
+                } else {
+
+                    // permission denied, boo! Disable the functionality that depends on this permission.
+                    Toast.makeText(TakePhotoActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    protected void createImageFileAndPassItAsUriToOnActivityResult(){
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(TakePhotoActivity.this,
+                    BuildConfig.APPLICATION_ID + ".provider", photoFile);
+
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
     private File createImageFile() throws IOException {
